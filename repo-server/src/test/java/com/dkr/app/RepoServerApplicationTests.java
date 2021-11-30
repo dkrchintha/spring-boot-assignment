397cd5381d1e3b81bdb7d26ab1e6a1c82a4c6e3d
Dukendra Kumar Reddy Chintha <dkreddy@DKReddy.attlocal.net>
2021-11-30 13:03:16 -0600

    Intial Commit

--- repo-server/src/test/java/com/dkr/app/RepoServerApplicationTests.java
+++ repo-server/src/test/java/com/dkr/app/RepoServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.ribbonserver;
+package com.dkr.app;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class RibbonServerApplicationTests {
+public class RepoServerApplicationTests {
 
 	@Test
 	public void contextLoads() {
